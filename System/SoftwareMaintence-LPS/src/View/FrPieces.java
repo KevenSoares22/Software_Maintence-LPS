003f60faac1aa2ed09e7561bb10551cb7cd29750
MatheusFaier <matheusalcfaier@gmail.com>
2024-12-26 15:47:59 -0300

    Criação da Model

--- System/SoftwareMaintence-LPS/src/View/FrPieces.java
+++ System/SoftwareMaintence-LPS/src/View/FrPieces.java
@@ -3,9 +3,9 @@ package View;
 import javax.swing.*;
 
 public class FrPieces {
-    private JButton button1;
+    private JButton criarButton;
     private JTextField textField1;
-    private JButton button2;
+    private JButton fecharButton;
     private JTextField textField2;
     private JTextField textField3;
     private JTextField textField4;
