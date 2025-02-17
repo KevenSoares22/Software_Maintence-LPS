197fb1194853393282ae5f7f8aca28e040324920
KevenSoares22 <keven.v.soares@gmail.com>
2025-02-17 18:12:42 -0300

    JPA Login

--- System/SafeSoft/src/main/java/com/ifcolab/safesoft/model/exceptions/RecepcionistaException.java
+++
@@ -1,7 +0,0 @@
-package com.ifcolab.safesoft.model.exceptions;
-
-public class RecepcionistaException extends ValidateException {
-    public RecepcionistaException(String msg) {
-        super(msg);
-    }
-} 
